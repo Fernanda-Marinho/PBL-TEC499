47e5616f3e2057f9c763eea2a8f06c48cc4f411d
Fernanda-Marinho <fmarinho1918@gmail.com>
2024-09-29 04:57:20 -0300

    refactor the main code of vga

--- tetris-leg/vga.c
+++ tetris-leg/vga.c
@@ -18,116 +18,7 @@
 #define BLOCK_SIZE 11
 
 
-//points se refere a pontuação, por isso alteracao da funcao 
-void drawMonitor(char* points, int rows, int cows, int matriz[rows][cows], int rows2, int cows2, int currentPiece, int tetrominoes[currentPiece][rows2][cows2], int currentX, int currentY, int nextPiece){
-    
-    if(video_open() == 1){
-        video_clear();
-        video_erase();
-
-        video_box(88, 0, 208, 239, video_WHITE); // desenha o retangulo branco lá
-        
-
-        // desenha a matriz principal
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cows; j++){
-                switch(matriz[i][j]){
-                   
-                    case 1: // desenha a peça da matriz do jogo
-                        video_box(
-                            (j*BLOCK_SIZE+90+(j-1)), 
-                            (i*BLOCK_SIZE+(i-1)), 
-                            (j*BLOCK_SIZE+10+90+(j-1)), 
-                            (i*BLOCK_SIZE+10+(i-1)), video_ORANGE); // cor da peca
-                        continue;
-                    
-                }
-            }
-        }
-        
-        // O PROBLEMA ESTÁ AQUI!!!!!!
-        // Desenha a peça temporariamente (antes de colidir)
-        int pieceHeight = (currentPiece == 1) ? 2 : 4;
-        for (int i = 0; i < pieceHeight; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (tetrominoes[currentPiece][i][j]) {
-                    video_box(
-                            (j*BLOCK_SIZE+(BLOCK_SIZE*currentX+currentX)+90+(j-1)), 
-                            (i*BLOCK_SIZE+(BLOCK_SIZE*currentY+currentY)+(i-1)), 
-                            (j*BLOCK_SIZE+(BLOCK_SIZE*currentX+currentX)+10+90+(j-1)), 
-                            (i*BLOCK_SIZE+(BLOCK_SIZE*currentY+currentY)+10+(i-1)), 
-                            video_GREEN);
-                }
-            }
-        }
-
-        // desenha a proxima peca no tabuleiro
-        video_text(60, 25, "SCORE: ");
-        video_text(70, 25, points);
-        video_box(240,30,290,80, video_GREY);
-        int nextPieceHeight = sizeof(tetrominoes[nextPiece]) / sizeof(tetrominoes[nextPiece][0]);
-        video_text(60, 5, "NEXT: ");
-        for (int i = 0; i < nextPieceHeight; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (tetrominoes[nextPiece][i][j]) {
-                    video_box(
-                    (j*BLOCK_SIZE+250+(j-1)), // x1
-                    (i*BLOCK_SIZE+40+(i-1)),  // y1
-                    (j*BLOCK_SIZE+10+250+(j-1)), // x2
-                    (i*BLOCK_SIZE+40+10+(i-1)), // y2
-                    video_PINK); // cor do bloco
-                }
-            }
-        }
-
-        
-        video_show();                
-        video_close();
-
-
-    } else {
-        printf("Video nao conectado!\n");
-    }
-}
-
-void gamePaused(){
-    video_open();
-    video_box(75, 40, 220, 100, video_GREY); // desenha o retangulo lá
-    video_text(33, 15, "PAUSED");
-    video_text(28, 20, "press KEY0 to resume");
-    video_show();
-    video_close();
-}
-
-void gameOver(){
-    video_open();
-    video_box(75, 40, 220, 100, video_GREY); // desenha o retangulo lá
-    video_text(30, 15, "GAME OVER!");
-    video_show();
-    video_open();
-    video_box(75, 40, 220, 100, video_GREY); // desenha o retangulo lá
-    video_text(30, 15, "GAME OVER!");
-    video_show();
-    video_close();
-}
-
-void mainWindow(){
-    video_open();
-    // clearVGA();
-    // video_box(100, 10, 130, 20, video_RED);
-    video_box(80, 40, 240, 100, video_GREY); // desenha o retangulo lá
-    video_text(33, 15, "WELCOME, PLAYER!");
-    // video_box(100, 10, 133, 20, video_RED);
-    video_text(30, 20, "PRESS KEY1 TO START!!!");
-
-    video_box(80, 40, 240, 100, video_GREY); // desenha o retangulo lá
-    video_text(33, 15, "WELCOME, PLAYER!");
-    video_text(30, 20, "PRESS KEY1 TO START!!!");
-
-    video_show();
-    video_close();
-}
-
+/* Clears the buffers of monitor  */
 void clearVGA(){
     video_open();
     video_erase();
@@ -139,3 +30,113 @@ void clearVGA(){
     video_show();
     video_close();
 }
+
+/* Renders the main game state on the screen, including the game grid, 
+the active piece, and the next piece. It also displays the player's score. 
+Receives the player's score, the dimensions of the game grid (rows and columns), 
+a 2D array representing the game grid, the dimensions of the tetromino pieces, 
+the index of the current piece, a 3D array of all pieces, the x and y coordinates 
+of the current piece on the grid, and the index of the next piece.      */ 
+void drawMonitor(char* points, int rows, int cols, int matriz[rows][cols], int rows2, int cols2, int currentPiece, int tetrominoes[currentPiece][rows2][cols2], int currentX, int currentY, int nextPiece){
+    
+    if(video_open() == 1){
+        
+        video_clear();
+        video_erase();
+        video_box(88, 0, 208, 239, video_WHITE);
+        
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                switch(matriz[i][j]){ // main matrix of the game
+                    case 1: // current piece
+                        video_box(
+                            (j*BLOCK_SIZE+90+(j-1)), 
+                            (i*BLOCK_SIZE+(i-1)), 
+                            (j*BLOCK_SIZE+10+90+(j-1)), 
+                            (i*BLOCK_SIZE+10+(i-1)), video_ORANGE);
+                        continue;
+                    
+                }
+            }
+        }
+
+        int pieceHeight = (currentPiece == 1) ? 2 : 4;
+        for (int i = 0; i < pieceHeight; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (tetrominoes[currentPiece][i][j]) { // renders the current piece 
+                    video_box(
+                            (j*BLOCK_SIZE+(BLOCK_SIZE*currentX+currentX)+90+(j-1)), 
+                            (i*BLOCK_SIZE+(BLOCK_SIZE*currentY+currentY)+(i-1)), 
+                            (j*BLOCK_SIZE+(BLOCK_SIZE*currentX+currentX)+10+90+(j-1)), 
+                            (i*BLOCK_SIZE+(BLOCK_SIZE*currentY+currentY)+10+(i-1)), 
+                            video_GREEN);
+                }
+            }
+        }
+
+        video_text(60, 25, "SCORE: "); //displays the score 
+        video_text(70, 25, points);
+
+        video_box(240,30,290,80, video_GREY);
+        video_text(60, 5, "NEXT: ");
+        for (int i = 0; i < nextPieceHeight; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (tetrominoes[nextPiece][i][j]) { // displays the next piece 
+                    video_box(
+                    (j*BLOCK_SIZE+250+(j-1)), 
+                    (i*BLOCK_SIZE+40+(i-1)),  
+                    (j*BLOCK_SIZE+10+250+(j-1)), 
+                    (i*BLOCK_SIZE+40+10+(i-1)), 
+                    video_PINK); 
+                }
+            }
+        }
+
+        video_show();                
+        video_close();
+
+    } else {
+        printf("ERROR: video not connected.\n");
+    }
+}
+
+/* Displays the main welcome screen for the game. Shows text prompting 
+the player to press a key to start the game.   */
+void mainWindow(){
+    video_open();
+    video_box(80, 40, 240, 100, video_GREY);
+    video_text(33, 15, "WELCOME, PLAYER!");
+    video_text(30, 20, "PRESS KEY1 TO START!!!");
+    video_box(80, 40, 240, 100, video_GREY); 
+    video_text(33, 15, "WELCOME, PLAYER!");
+    video_text(30, 20, "PRESS KEY1 TO START!!!");
+    video_show();
+    video_close();
+}
+
+/* Informes the player that the game is paused and providing instructions 
+on how to resume.  */
+void gamePaused(){
+    video_open();
+    video_box(75, 40, 220, 100, video_GREY); 
+    video_text(33, 15, "PAUSED");
+    video_text(28, 20, "press KEY0 to resume");
+    video_show();
+    video_close();
+}
+
+/* Appears when a collision occurs at the top, notifying the player 
+that the game has ended.   */
+void gameOver(){
+    video_open();
+    video_box(75, 40, 220, 100, video_GREY); 
+    video_text(30, 15, "GAME OVER!");
+    video_show();
+    video_open();
+    video_box(75, 40, 220, 100, video_GREY);
+    video_text(30, 15, "GAME OVER!");
+    video_show();
+    video_close();
+}
+
+
--- tetris-leg/vga.h
+++ tetris-leg/vga.h
@@ -6,7 +6,7 @@
 void clearVGA();
 
 /* Game display Functions  */
-void drawMonitor(char* points, int rows, int cows, int matriz[rows][cows], int rows2, int cows2, int currentPiece, int tetrominoes[currentPiece][rows2][cows2], int currentX, int currentY, int nextPiece);
+void drawMonitor(char* points, int rows, int cols, int matriz[rows][cols], int rows2, int cols2, int currentPiece, int tetrominoes[currentPiece][rows2][cols2], int currentX, int currentY, int nextPiece);
 void mainWindow();
 
 /* Game mode Functions  */
